150e023f6e82c2d0ebd7ad7891310e5a471ee2b4
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-05-21 23:46:10 +0100

    Firebase CRUD setup  [Reads the DB]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
+import { db } from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const moviesCollectionRef = collection(db, "movies");
+  useEffect(() => {
+    const getMovieList = async () => {
+      // READ DATA FROM DB
+      // SET MOVIELIST STATE
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((docs) => ({
+          ...docs.data(),
+          id: docs.id,
+        }));
+        setMovieList(filteredData);
+        console.log(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getMovieList();
+  }, []);
+
   return (
     <>
       Hello
       <Auth />
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h2>Title : {movie.Title}</h2>
+            <p>Release date : {movie.Release}</p>
+            <p>Award winning : {movie.AwardWinning ? "Yes" : "No"}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
